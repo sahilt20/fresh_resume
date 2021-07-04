833bd29f2925db9a2a52979e4ba3f5b212b1bd79
Sahil Tanwar <44357595+sahilt20@users.noreply.github.com>
2021-07-04 16:25:56 +0530

    Update gatsby-config.js
    name changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,12 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Sahil Chiang',
+    title: 'Sahil Tanwar',
     description:
-      'Sahil Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://Sahilchiang.com', // No trailing slash allowed!
+      'Sahil Tanwar is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
+    siteUrl: 'https://sahil-tanwar.herokuapp.com/', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '@Sahiltanwar',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -20,8 +20,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'SahilChiang',
-        short_name: 'SahilChiang',
+        name: 'SahilTanwar',
+        short_name: 'SahilTanwar',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
